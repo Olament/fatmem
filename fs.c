fd0cd37be16aa129621f86c5ddf88e7f41a20f91
Zixuan Guo <guozixuan2000@gmail.com>
2022-05-10 13:22:55 -0500

    code format

--- fs.c
+++ fs.c
@@ -12,11 +12,11 @@ typedef struct __attribute__((__packed__)) data_block {
 } data_block_t;
 
 typedef struct node {
-    struct node* next;
+    struct node *next;
 } node_t;
 
-node_t* free_list = NULL;
-data_block_t* blocks = NULL;
+node_t *free_list = NULL;
+data_block_t *blocks = NULL;
 
 file_entry_t entries[MAX_FILE_SIZE];
 size_t next_entry = 0;
@@ -27,14 +27,13 @@ void fs_init() {
 
     // adding data blocks to the free list
     for (size_t i = 0; i < MAX_BLOCK_SIZE; i++) {
-        node_t* curr_node = (node_t*)(&blocks[i]);
+        node_t *curr_node = (node_t *)(&blocks[i]);
         curr_node->next = free_list;
         free_list = curr_node;
     }
 }
 
-int open(const char* file_name) {
-
+int open(const char *file_name) {
     if (next_entry >= MAX_FILE_SIZE) {
         return false;
     }
@@ -46,7 +45,7 @@ int open(const char* file_name) {
             return fd;
         }
     }
-    
+
     // file not found, create a new file instead
     // sanity check
     if (next_entry >= MAX_FILE_SIZE) {
@@ -59,7 +58,7 @@ int open(const char* file_name) {
     strcpy(entries[next_entry].name, file_name);
     entries[next_entry].file_size = 0;
     entries[next_entry].start_index = ((uintptr_t)free_list - (uintptr_t)blocks) / BLOCK_SIZE;
-    data_block_t* new_block = free_list;
+    data_block_t *new_block = free_list;
     new_block->index = -1;
     free_list = free_list->next;
 
@@ -73,24 +72,20 @@ size_t min(size_t num1, size_t num2) { return num1 > num2 ? num2 : num1; }
 
 size_t min(size_t num1, size_t num2) { return num1 > num2 ? num2 : num1; }
 
-bool write(int fd, uint8_t* buf, size_t size) {
-    write_at(fd, 0, buf, size);
-}
-
+bool write(int fd, uint8_t *buf, size_t size) { write_at(fd, 0, buf, size); }
 
 void write_at(int fd, size_t index, uint8_t buff, size_t size) {
-    file_entry_t* entry = find_fd(fd);
+    file_entry_t *entry = find_fd(fd);
     size_t fsize = entry->file_size;
 
-    if(index > fsize)
-        return;
+    if (index > fsize) return;
 
-    int num_blocks = (fsize + BLOCK_DATA_SIZE -1 ) / BLOCK_DATA_SIZE;
-    int blocks_needed = (index + size + BLOCK_DATA_SIZE - 1 ) / BLOCK_DATA_SIZE;
+    int num_blocks = (fsize + BLOCK_DATA_SIZE - 1) / BLOCK_DATA_SIZE;
+    int blocks_needed = (index + size + BLOCK_DATA_SIZE - 1) / BLOCK_DATA_SIZE;
 
-    if(blocks_needed > num_blocks){ // need allocate new block
+    if (blocks_needed > num_blocks) {  // need allocate new block
         // go to the end of data block of file
-        data_block_t* curr_block = &blocks[entry->start_index];
+        data_block_t *curr_block = &blocks[entry->start_index];
         while (curr_block->index != -1) {
             curr_block = &blocks[curr_block->index];
         }
@@ -103,7 +98,7 @@ void write_at(int fd, size_t index, uint8_t buff, size_t size) {
                 return;
             }
 
-            data_block_t* new_block = free_list;
+            data_block_t *new_block = free_list;
             free_list = free_list->next;
 
             curr_block->index = ((uintptr_t)new_block - (uintptr_t)blocks) / BLOCK_SIZE;
@@ -116,41 +111,38 @@ void write_at(int fd, size_t index, uint8_t buff, size_t size) {
         }
     }
 
-
     // Skip number of blocks to get to index to start writing at.
     size_t start_block = index / BLOCK_DATA_SIZE;
     size_t write_point = index % BLOCK_DATA_SIZE;
 
     // Loop through each block to get to desired
-    data_block_t* curr_block = &blocks[entry->start_index];
-    for(int i = 0; i < start_block; i++) {  // EOF
+    data_block_t *curr_block = &blocks[entry->start_index];
+    for (int i = 0; i < start_block; i++) {  // EOF
         curr_block = &blocks[curr_block->index];
     }
 
     // Once we are at block, start writing byte by byte
-    while(size > 0){
+    while (size > 0) {
         // change write point if we reach the end
         if (write_point == BLOCK_DATA_SIZE) {
             curr_block = &blocks[curr_block->index];
-            write_point = 0; 
+            write_point = 0;
         }
 
         memcpy((curr_block->data) + write_point++, buff++, 1);
         size--;
     }
 }
-    
 
-
-bool append(int fd, uint8_t* buf, size_t size) {
-    file_entry_t* entry = find_fd(fd);
+bool append(int fd, uint8_t *buf, size_t size) {
+    file_entry_t *entry = find_fd(fd);
     write_at(fd, entry->file_size, buf, size);
 }
 
 void read(int fd) {
-    file_entry_t* entry = find_fd(fd);
+    file_entry_t *entry = find_fd(fd);
     size_t total_size = entry->file_size;
-    data_block_t* curr_block = &blocks[entry->start_index];
+    data_block_t *curr_block = &blocks[entry->start_index];
 
     for (;;) {
         for (size_t j = 0; j < min(BLOCK_DATA_SIZE, total_size); j++) {
@@ -164,18 +156,17 @@ void read(int fd) {
         }
         curr_block = &blocks[curr_block->index];
     }
-    
 }
 
 void delete (int fd) {
-    file_entry_t* entry = find_fd(fd);
+    file_entry_t *entry = find_fd(fd);
 
     // first block
-    data_block_t* curr_block = &blocks[entry->start_index];
-    node_t* curr_node;
+    data_block_t *curr_block = &blocks[entry->start_index];
+    node_t *curr_node;
     while (curr_block->index != -1) {  // EOF
-        //cast to node
-        node_t* curr_node = (node_t*)curr_block;
+        // cast to node
+        node_t *curr_node = (node_t *)curr_block;
         curr_node->next = free_list;
         free_list = curr_node;
         curr_block = &blocks[curr_block->index];
@@ -186,7 +177,7 @@ void delete (int fd) {
     memset(&entry->file_size, 0, sizeof(size_t));
 }
 
-bool rename(const char* oldpath, const char* newpath) {
+bool rename(const char *oldpath, const char *newpath) {
     for (int i = 0; i < next_entry; i++) {
         if (strcmp(oldpath, entries[i].name) == 0) {
             strcpy(entries[i].name, newpath);
